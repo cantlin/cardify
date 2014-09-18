86ea16dc093b83f2d20ea0918989aa7482169f2c
Cantlin Ashrowan <cantlin@ashrowan.com>
2014-09-18 21:10:14 +0100

    clean up api example

--- example/api.rb
+++ example/api.rb
@@ -9,61 +9,78 @@ set :port, CONF['port']
 set :bind, CONF['bind']
 set :protection, :except => :frame_options
 
-MEMCACHE_HOST = 'localhost:11211'
-CACHE = Dalli::Client.new(MEMCACHE_HOST, { :namespace => "cardify:10", :compress => true, :expires_in => 60})
+# memcached all the things
+CACHE = Dalli::Client.new('localhost:11211', {
+	:namespace => "cardify:10",
+	:compress => true,
+	:expires_in => 60
+})
 
-class ContentAPI
-	def self.content_for_path path
-		path.gsub! %r!https?:/?/www.theguardian.com/?!, ""
+class Content
+	attr_accessor :url, :title, :published, :trailtext, :byline
+	attr_accessor :content_type, :tone, :byline_image, :author_name
+	attr_accessor :author_url, :thumb
+
+	def initialize path
 		content = CACHE.get path
-		return self.oembed(content) if content
+		return self.build(content) if content
 
-		uri = "#{CONF['content_api_host']}/#{path}?api-key=#{CONF['content_api_key']}&show-tags=tone,type,contributor&show-fields=headline,byline,trailText&show-elements=all"
+		uri  = "#{CONF['content_api_host']}/#{path}?api-key=#{CONF['content_api_key']}&show-tags=tone,type,contributor&show-fields=headline,byline,trailText&show-elements=all"
 		req  = Curl.get uri
-		data = JSON.parse req.body_str
-		response = data['response']['content']
-		CACHE.set path, response
-		self.oembed response
+		data = JSON.parse(req.body_str)['response']['content']
+
+		CACHE.set path, data
+		self.build(data)
 	end
 
-	def self.oembed api_response
+	# flatten an API response into a simple object
+	def build data
+		tones = data['tags'].select {|t| t['type'] == 'tone'}.map {|t| t['id'].gsub('tone/', '')}
+		contributors = data['tags'].select {|t| t['type'] == 'contributor'}
+		elements = data['elements']
+
+		self.url = data['webUrl']
+		self.title = data['fields']['headline']
+		self.published = data['webPublicationDate']
+		self.trailtext = data['fields']['trailText']
+		self.byline = data['fields']['byline']
+		self.content_type = data['tags'].find {|t| t['type'] == 'type'}['id'].gsub('type/', '')		
+		self.tone = tones.find {|t| t == 'minutebyminute'} || tones.find {|t| t == 'comment'} || tones.find {|t| t == 'features' } || 'news'
+
+		if contributors.length == 1
+			self.byline_image = contributors[0]['bylineLargeImageUrl']
+			self.author_name = contributors[0]['webTitle']
+			self.author_url = contributors[0]['webUrl']
+		end
+
+		# deriving a thumb is torturous
+		thumb_max_size = 380
+		begin
+			main_element = elements.find {|e| e['relation'] == 'main' }
+			self.thumb = main_element['assets'].max_by {|a| w = a['typeData']['width'].to_i; w >= thumb_max_size ? 0 : w }['file']
+		rescue
+			begin
+				self.thumb = elements.find {|e| e['type'] == 'video' }['assets'][0]['typeData']['stillImageUrl']
+			rescue
+				begin
+					self.thumb = elements.find {|e| e['relation'] == 'gallery' }['assets'][0]['file']
+				rescue; end
+			end
+		end
+	end
+
+	def to_oembed
 		content = {
 			provider_name: "The Guardian",
 			provider_url: "http://www.theguardian.com/",
 			version: "1.0", # oembed version – required
-			type: "link",
-			url: api_response['webUrl'],
-			title: api_response['fields']['headline'],
-			published: api_response['webPublicationDate'],
-			trailText: api_response['fields']['trailText'],
-			byline: api_response['fields']['byline']
+			type: "link"
 		}
 
-		content[:content_type] = api_response['tags'].select {|t| t['type'] == 'type'}[0]['id'].gsub('type/', '')
-		tones = api_response['tags'].select {|t| t['type'] == 'tone'}.map {|t| t['id'].gsub('tone/', '')}
-		content[:tone] = tones.find {|t| t == 'minutebyminute'} || tones.find {|t| t == 'comment'} || tones.find {|t| t == 'features' } || 'news'
-		contributors = api_response['tags'].select {|t| t['type'] == 'contributor'}
-
-		if contributors.length == 1
-			content[:byline_image] = contributors[0]['bylineLargeImageUrl']
-			content[:author_name] = contributors[0]['webTitle']
-			content[:author_url] = contributors[0]['webUrl']
-		end
-
-		elements = api_response['elements']
-		begin
-			main_element = elements.find {|e| e['relation'] == 'main' }
-			content[:thumb] = main_element['assets'].max_by {|a| w = a['typeData']['width'].to_i; w >= 380 ? 0 : w }['file']
-		rescue
-			begin
-				content[:thumb] = elements.find {|e| e['type'] == 'video' }['assets'][0]['typeData']['stillImageUrl']
-			rescue
-				begin
-					content[:thumb] = elements.find {|e| e['relation'] == 'gallery' }['assets'][0]['file']
-				rescue
-				end
+		[:url, :title, :published, :trailtext, :byline, :content_type,
+			:tone, :byline_image, :author_name, :author_url].each do |k|
+				content[k] = self.send(k)
 			end
-		end
 
 		content
 	end
@@ -71,7 +88,7 @@ end
 
 get %r{/html/(.+)} do
   path = params[:captures][0]
-  content = ContentAPI.content_for_path(path)
+  content = Content.new path
 
   erb :html_embed, :locals => { :content => content }
 end
@@ -80,13 +97,14 @@ get %r{/oembed/(.+)} do
   content_type "application/javascript"
 
   path = params[:captures][0]
-  content = ContentAPI.content_for_path(path)
-  content['embed_link'] = "http://#{CONF['host']}:#{CONF['port']}/html/#{path}"
-  content['stub'] = CGI.escapeHTML(erb(:stub, :locals => { :content => content }))
+  oembed = Content.new(path).to_oembed
+
+  oembed['embed_link'] = "http://#{CONF['host']}:#{CONF['port']}/html/#{path}"
+  oembed['stub'] = CGI.escapeHTML(erb(:stub, :locals => { :content => oembed }))
 
   if params[:callback]
-  	"#{params[:callback]}(#{content.to_json})"
+  	"#{params[:callback]}(#{oembed.to_json})"
   else
-  	content.to_json
+  	oembed.to_json
   end
 end
